47b6a6aefd4e966e8f25c5b3e9aac98a408fd619
Bart Willems <bwillems@protonmail.com>
2019-03-22 10:18:54 +0100

    Test if a user tries to do a payment to himself
    Signed-off-by: Bart Willems <bwillems@protonmail.com>

--- pkg/api/transactions.go
+++ pkg/api/transactions.go
@@ -15,6 +15,11 @@ func pay(c *gin.Context) {
 		return
 	}
 
+	if transaction.Sender == transaction.Receiver {
+		c.JSON(http.StatusBadRequest, gin.H{"failure_reason": "You can not send money to yourself"})
+		return
+	}
+
 	if err := db.Save(&transaction); err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"failure_reason": err.Error()})
 		return
