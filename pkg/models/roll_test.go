0241d8c096763391ab150914b0e21f02ed674afb
Bart Willems <bwillems@protonmail.com>
2019-04-09 23:43:38 +0200

    Fixed the new faling /roll tests (very test driven right?)
    Signed-off-by: Bart Willems <bwillems@protonmail.com>

--- pkg/models/roll_test.go
+++ pkg/models/roll_test.go
@@ -7,20 +7,20 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func Test_Transaction(t *testing.T) {
+func Test_RollRewards(t *testing.T) {
 	roll := RollReward{
 		GroupID:    1,
 		Receiver:   1,
 		Multiplier: nulls.NewUInt32(0),
 	}
 	// Dubs
-	assert.Equal(t, 20, roll.GetAmount())
+	assert.EqualValues(t, 20, roll.GetAmount())
 
 	// Trips
 	roll.Multiplier = nulls.NewUInt32(1)
-	assert.Equal(t, 200, roll.GetAmount())
+	assert.EqualValues(t, 200, roll.GetAmount())
 
 	// Quads
 	roll.Multiplier = nulls.NewUInt32(2)
-	assert.Equal(t, 2000, roll.GetAmount())
+	assert.EqualValues(t, 2000, roll.GetAmount())
 }
