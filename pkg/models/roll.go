96fdba0d86f068832af6acdcd9330b6cde9dc783
Bart Willems <bwillems@protonmail.com>
2019-04-09 14:21:46 +0200

    Changed the roll multiplier to a nullable int to fix the binding validation when multiplier == 0
    Signed-off-by: Bart Willems <bwillems@protonmail.com>

--- pkg/models/roll.go
+++ pkg/models/roll.go
@@ -9,9 +9,9 @@ import (
 
 // RollReward is a struct used for creating roll rewards
 type RollReward struct {
-	GroupID    int64 `json:"group_id" binding:"required"`
-	Receiver   int   `json:"receiver" binding:"required"`
-	Multiplier uint8 `json:"multiplier" binding:"required"`
+	GroupID    int64        `json:"group_id" binding:"required"`
+	Receiver   int          `json:"receiver" binding:"required"`
+	Multiplier nulls.UInt32 `json:"multiplier" binding:"required"`
 }
 
 // Create maps a reward to a real transaction and creates it
@@ -37,17 +37,17 @@ func (r *RollReward) Create(tx *pop.Connection) (*Transaction, error) {
 
 // GetAmount returns the amount of kalicoins a user should receive for his /roll attempt
 func (r *RollReward) GetAmount() uint32 {
-	return (uint32(r.Multiplier) + 1) * 10 * PriceTable[Payment]["roll"]
+	return (r.Multiplier.UInt32 + 1) * 10 * PriceTable[Payment]["roll"]
 }
 
 // GetCause returns the name for a certain multiplier
 // eg dubs for 0
 func (r *RollReward) GetCause() (string, error) {
-	if r.Multiplier > 8 {
+	if r.Multiplier.UInt32 > 8 {
 		return "", errors.New("Invalid Multiplier, must be within ]0-8[")
 	}
 
 	names := [9]string{"dubs", "trips", "quads", "penta", "hexa", "septa", "octa", "el niño"}
 
-	return names[r.Multiplier], nil
+	return names[r.Multiplier.UInt32], nil
 }
